a9b482b274848d99d093c84f803c70fd1b170128
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 18:25:21 +0800

    Remove indent config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     ecmaVersion: 2017,
   },
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
