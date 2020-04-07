51cb03af60c1247b141995a756760ee8d798e8a2
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 18:25:00 +0800

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['prettier'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2017,
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
