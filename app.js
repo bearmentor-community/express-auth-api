984daecf3a596d176d3e73655657584e00052645
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 19:19:40 +0800

    Fix require cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users/index')
