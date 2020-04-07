c1fb5b475e7996941f3f545d22a946c9d63c7eb4
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 17:45:07 +0800

    Remove static folder and config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+  res.send({
+    message: `Hello!`,
+  })
+})
 
-module.exports = router;
+module.exports = router
