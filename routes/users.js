fd8db41fce577b4c8348c82057a83c7bd98426d3
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 17:58:54 +0800

    Change var to const

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.send({
     message: `Users`,
     data: [],
