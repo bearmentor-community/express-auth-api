9011921251d38a0ed63ec60ffb79c3230bc330c3
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-09 00:29:40 +0800

    Add documentation link in index

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   res.status(200).send({
     message: `Project Auth Express`,
+    documentation: `https://api.haidar.dev/docs`,
   })
 })
 
