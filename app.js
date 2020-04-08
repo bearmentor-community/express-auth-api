9c024b2602bc789ac60ab8f578ee7883d52f87e5
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-08 19:58:32 +0800

    Setup middlewares usage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users/index')
+const indexMiddleware = require('./middlewares/index')
+const usersMiddleware = require('./middlewares/users/index')
 
 const app = express()
 
@@ -15,7 +17,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/', indexMiddleware)
+app.use('/users', usersMiddleware)
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500).send({
+    error: err,
+  })
+})
 
 module.exports = app
