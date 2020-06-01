916be3f9a5d93fa0ed2c0dd41df753e51c704c09
M Haidar Hanif <git@mhaidarhanif.com>
2020-06-02 03:12:08 +0800

    Update Express configuration

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 
-const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 
-const indexMiddleware = require('./middlewares/index')
-const usersMiddleware = require('./middlewares/users/index')
+const indexMiddleware = require('./middlewares')
+const authMiddleware = require('./middlewares/auth')
+const usersMiddleware = require('./middlewares/users')
 
 const app = express()
 
@@ -21,19 +21,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexMiddleware)
+app.use('/auth', authMiddleware)
 app.use('/users', usersMiddleware)
-
-const swaggerDocument = YAML.load('./docs/swagger.yaml')
-app.use(
-  '/docs',
-  (req, res, next) => {
-    swaggerDocument.host = req.get('host')
-    req.swaggerDoc = swaggerDocument
-    next()
-  },
-  swaggerUi.serve,
-  swaggerUi.setup()
-)
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message
